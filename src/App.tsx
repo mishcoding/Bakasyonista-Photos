09819c596151d238a1f922975ee1ec33c2a8f10b
mishcoding <mkartworks12@gmail.com>
2026-02-24 21:00:32 -0600

    fixed supabase import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://ydwkakmfwdvkardywfy.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlkd2tha21tZndkdmthcmR5d2Z5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5ODI0NTYsImV4cCI6MjA4NzU1ODQ1Nn0.bCiyzdHEbjZSlKh6PeNSOIPhWLmqFcxuyEr5IB90tgs";
